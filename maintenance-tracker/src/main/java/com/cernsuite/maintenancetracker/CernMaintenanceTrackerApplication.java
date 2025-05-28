1b82aa261b07e9146e12f06512e66b5575959646
Siddharth <siddharthajith1717@gmail.com>
2025-05-28 20:17:41 +0530

    chore: initial project setup with basic Spring Boot structure

---
+++ maintenance-tracker/src/main/java/com/cernsuite/maintenancetracker/CernMaintenanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cernsuite.maintenancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CernMaintenanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CernMaintenanceTrackerApplication.class, args);
+	}
+
+}
---
+++ maintenance-tracker/src/test/java/com/cernsuite/maintenancetracker/CernMaintenanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cernsuite.maintenancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CernMaintenanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
